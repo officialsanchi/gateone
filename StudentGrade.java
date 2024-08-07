2119f82bd769ed5502bedc540f1c317bf4865a6b
officialsanchi <nwangwudinma22@gmail.com>
2024-08-07 09:02:06 +0100

    commit

---
+++ StudentGrade.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class StudentGrade{
+	public static void main(String [] args){
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("NAME OF SCHOOL");
+	System.out.println("LAGBAJA SCHOOL");
+	
+	System.out.print("Name of teacher : ");
+	String name = input.nextLine();
+
+	System.out.print("Enter number of student : ");
+	int numberOfStudent = input.nextInt();
+	System.out.print("Enter number of subject : ");
+	int numberOfSubject = input.nextInt();
+
+
+	int [][]  nameAndSubject = new int [numberOfStudent][numberOfSubject];
+	double [] average = new double [numberOfSubject];
+	
+
+	for(int count = 0, student = 1 ; count < numberOfStudent; count++, student++){
+	int sum = 0;
+		for(int counter = 0, subject = 1 ;  counter < numberOfSubject; counter ++ ,subject++){
+		System.out.println("student "+ student+"\n subject"+subject+ ":");
+		nameAndSubject [count][counter] = input.nextInt();
+
+		sum += nameAndSubject [count][counter] ;
+		
+		average [counter] =  sum / numberOfSubject ;
+		
+		
+
+		
+		
+		System.out.println(sum);
+		
+		System.out.println();
+		} 
+			}
+		
+System.out.println(Arrays.toString (average));
+	    }
+}
