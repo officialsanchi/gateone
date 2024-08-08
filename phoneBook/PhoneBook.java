20150014670c78b6cd65509e09e732f267d679d5
officialsanchi <nwangwudinma22@gmail.com>
2024-08-08 16:55:17 +0100

    commit

--- phoneBook/PhoneBook.java
+++ phoneBook/PhoneBook.java
@@ -3,64 +3,53 @@ import java.util.ArrayList;
 public class PhoneBook{
 public static void main (String [] args){
 	Scanner collect = new Scanner(System.in);
-	ArrayList<String> contacts = new ArrayList<String>();
-	System.out.println("""
+	ArrayList<String> addContact = new ArrayList<String>();
+	
+	
 
-			MENU
-			[1] ADD CONTACT
-			[2] REMOVE CONTACT
-			[3] FIND CONTACT BY FIRST NAME
-			[4] FIND CONTACT BY FIRST NAME
-			[5] FIND CONTACT BY LAST NAME
-			[6] EDIT CONTACT
+		System.out.print("Enter first name :");
+		String firstName = collect.nextLine();
+		System.out.print("Enter last name :");
+		String lastName = collect.nextLine();
+		System.out.print("Enter email Address  :");
+		String emailAddress = collect.nextLine();
+		System.out.print("Enter contact Address :");
+		String contactAddress = collect.nextLine();
+		System.out.print("Enter phone number :");
+		String phoneNumber = collect.next();
 
-			""");
-	System.out.println("Enter 1 to continue or 0 exit");
+	System.out.print("Enter 1 to continue saving or 0 to exit");
+
 	int number = collect.nextInt();
-	switch(number){
-	case 1: System.out.println("""
-	
-	1.Name 
-	2.Address
-	3.Telephone
-	4.Email
+		while(number == 1){
+		System.out.print("Enter first name :");
+		 firstName = collect.next();
+		System.out.print("Enter last name :");
+		 lastName = collect.next();
+		System.out.print("Enter email Address  :");
+		emailAddress = collect.next();
+		System.out.print("Enter contact Address :");
+		contactAddress = collect.next();
+		System.out.print("Enter phone number :");
+		phoneNumber = collect.next();
+		
+		number++;
 
-""");
+	System.out.print("Enter 1 to continue \n 2 to back to main menu\n 0  to exit");
 
-
-if (number > 1){
-System.out.println(" firstname/tlastname/temailAddress/tcontactAddress/tphoneNumber");
-		System.out.println(" Enter first name :");
-		String firstName = collect.nextLine();
-		System.out.println("Enter last name :");
-		String lastName = collect.nextLine();
-		System.out.println(" Enter email Address  :");
-		String emailAddress = collect.nextLine();
-		System.out.println("Enter contact Address :");
-		String contactAddress = collect.nextLine();
-		System.out.println(" Enter phone number :");
-		String phoneNumber = collect.nextLine();
-
-	}
-	System.out.println("Enter 1 to continue or 0 exit");
-
-	
-	
-	System.out.println(" Enter first name :");
-		String firstName = collect.nextLine();
-	System.out.println("Enter last name :");
-		String lastName = collect.nextLine();
-	System.out.println(" Enter email Address  :");
-		String emailAddress = collect.nextLine();
-	System.out.println("Enter contact Address :");
-		String contactAddress = collect.nextLine();
-	System.out.println(" Enter phone number :");
-		String phoneNumber = collect.nextLine();
-
-	
+	number = collect.nextInt();
 	
 	}
 	
 
+	
+
+	
+		
+	
+
+	
+		
 	}
 }
+	
